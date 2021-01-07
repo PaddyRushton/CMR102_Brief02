a88937752ba63a2acfd96ae30d9cba03ec6a0c21
PaddyRushton <paddyrushton@gmail.com>
2021-01-07 23:48:45 +1100

    Changed particle effect parameters. The sun looks particularly cool now

--- Assets/Scripts/CanvasRotator.cs
+++ Assets/Scripts/CanvasRotator.cs
@@ -8,7 +8,7 @@ public class CanvasRotator : MonoBehaviour
     void Update()
     {
         Vector3 relativePosition = transform.position - Camera.main.transform.position; // get the relative direciton of the canvas to the players camera
-        //relativePosition.y = 0; // zero our y axis as we don't want it to rotate on that axis.
+        relativePosition.z = 0; // zero our y axis as we don't want it to rotate on that axis.
         Quaternion rotation = Quaternion.LookRotation(relativePosition); // create a quaternian rotation to look at the relative position specified above (The location of the Canvas in the world)
         transform.rotation = rotation; // Set the canvas rotation to the current LookRotation specified above.
     }
